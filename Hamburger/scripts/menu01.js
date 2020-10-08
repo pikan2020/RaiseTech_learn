0062ae95c8ee190120bb21c9d0c313a2a298ef78
makoto <MAKOTO@MacBook-1.local>
2020-10-08 18:29:48 +0900

    close_btn

--- Hamburger/scripts/menu01.js
+++ Hamburger/scripts/menu01.js
@@ -3,3 +3,9 @@ $('#menu-btn').click(function(){
     $('#content-wrapper').css('transform','translate(0%)');
     $('#l-sidebar').css('transform','translate(0%)');
 });
+
+$('#close__btn').click(function(){
+    $('#content-wrapper').css('width','0%');
+    $('#content-wrapper').css('transform','translate(100%)');
+    $('#l-sidebar').css('transform','translate(100%)');
+});
