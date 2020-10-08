00047320627083e897470d58e1a67c258b6eedd6
makoto <MAKOTO@MacBook-1.local>
2020-10-08 17:14:54 +0900

    jquery_add

---
+++ Hamburger/scripts/menu01.js
@@ -0,0 +1,5 @@
+$('#menu-btn').click(function(){
+    $('#content-wrapper').css('width','100%');
+    $('#content-wrapper').css('transform','translate(0%)');
+    $('#l-sidebar').css('transform','translate(0%)');
+});
